19e3acff07f82e91dc0cad29ad640a9c1c401903
nani6765 <nani6765@gmail.com>
2021-06-09 18:12:52 +0900

    hotfix

--- config/prod.js
+++ config/prod.js
@@ -1,5 +1,5 @@
 module.exports = {
   mongoURI: process.env.MONGO_URI,
-  access_key: process.env.ACCESS_KEY,
-  secret_key: process.env.SECRET_KEY,
+  access_key: process.env.S3_KEY,
+  secret_key: process.env.S3_SECRET,
 };
