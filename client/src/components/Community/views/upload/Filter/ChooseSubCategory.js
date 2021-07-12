0e440bf75334a89b8cbdb12597acc030b051f379
kimdoyoen <98ehdusdl@naver.com>
2021-07-12 17:28:43 +0900

    choose subcategory css

--- client/src/components/Community/views/upload/Filter/ChooseSubCategory.js
+++ client/src/components/Community/views/upload/Filter/ChooseSubCategory.js
@@ -1,28 +1,52 @@
 import React, { useState, useEffect } from 'react'
-//import { SubCategoryFilterDiv } from './UploadFilterContent.js';
+import { SubCategoryFilterDiv } from './UploadFilterContent.js';
 import Select from "react-select";
+import { symbol } from 'prop-types';
+import { AutoScaling } from 'aws-sdk';
 
 function ChooseSubCategory() {
-    const [SubCategory, setSubCategory] = useState("전체");
 
-    useEffect(() => {
-        console.log(SubCategory);
-    }, [SubCategory]);
+    const category = [
+        { value: 'all', label:'전체'},
+        { value: 'free', label:'자유게시판'},
+        { value: 'promotion', label:'홍보게시판'},
+        { value: 'qna', label:'질문게시판'},
+        { value: 'other', label:'기타'}
+    ]
 
-    function changeHandler(e) {
-        setSubCategory(e.target.value);
+    const selectStyles = {
+        control: (styles, {isSelected}) => ({...styles, textAlign: 'center', width: '13rem', height: '1rem', margin: '0px', border:isSelected ? '1px solid #702cba' : null}),
+        option: (styles, { isSelected }) => {
+            return {
+            width: '13rem',
+            height: '2rem',
+            lineHeight: '2rem',
+            backgroundColor: isSelected
+            ? 'rgba(17, 17, 17, 0.16)'
+            : null,
+            textAlign: 'center',
+            }
+        },
+        singleValue: styles => ({
+            ...styles,
+            width:'13rem',
+        }),
+        menuList: styles => ({
+            ...styles,
+            padding: '0px',
+        }),
+        menu: styles => ({
+            ...styles,
+            margin: '0px'
+        })
     }
 
     return (
         <>
-                <span>카테고리</span>
-                <select value={SubCategory} onChange={(e) => changeHandler(e)}>
-                    <option value="all">전체</option>
-                    <option value="free">자유게시판</option>
-                    <option value="promotion">홍보게시판</option>
-                    <option value="qna">질문게시판</option>
-                    <option value="other">기타</option>
-                </select>
+        <div style={{display:"flex", flexWrap:"nowrap", alignItems:"center"}}>
+            <p style={{marginBottom:"0px", marginRight:"1rem"}}>카테고리</p>
+            <Select options={category} defaultValue={category[0]} styles={selectStyles}/>    
+        </div>
         </>
     )
 }
