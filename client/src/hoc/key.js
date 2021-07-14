8189d8b48036a2da16a2c05ac6f08f5ff6036abb
nani6765 <nani6765@gmail.com>
2021-07-14 18:14:23 +0900

    20210714

---
+++ client/src/hoc/key.js
@@ -0,0 +1,6 @@
+const googleClientId =
+  "398748469422-09qrq11ip627468edpholpd3sofe4df3.apps.googleusercontent.com";
+const naverClientId = "U4uQz1lrtqwZRW2agLol";
+const kakaoClientId = "";
+
+export { googleClientId, naverClientId, kakaoClientId };
