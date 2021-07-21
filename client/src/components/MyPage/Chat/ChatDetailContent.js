6f469f8c88e17f298bd68bc040a90a3526ec282c
kimdoyoen <98ehdusdl@naver.com>
2021-07-21 16:03:52 +0900

    20210721

--- client/src/components/MyPage/Chat/ChatDetailContent.js
+++ client/src/components/MyPage/Chat/ChatDetailContent.js
@@ -1,12 +1,15 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { firebase } from "../../../firebase.js";
 import { useSelector } from "react-redux";
+import ChatUpload from './ChatUpload.js';
+import ImageUpload from "./ImageUpload.js";
 
 function ChatDetailContent(props) {
-  const [SendComment, setSendComment] = useState("");
   const [Comments, setComments] = useState([]);
   const [ChatRoomId, setChatRoomId] = useState("");
+
   const user = useSelector((state) => state.user);
+
   let MessageRef = firebase.database().ref("chats");
 
   //ChatRoomId 부모에게서 받아오기
@@ -33,21 +36,6 @@ function ChatDetailContent(props) {
     });
   };
 
-  const CreateMessage = (ChatRoomId) => {
-    MessageRef.child(ChatRoomId).push().set({
-      timestamp: firebase.database.ServerValue.TIMESTAMP,
-      username: user.userData.displayName,
-      profile_picture: user.userData.photoURL,
-      uid: user.userData.uid,
-      comment: SendComment,
-    });
-  };
-
-  const submitHandler = (e) => {
-    e.preventDefault();
-    CreateMessage(ChatRoomId);
-  };
-
   return (
     <>
       <div>
@@ -60,16 +48,9 @@ function ChatDetailContent(props) {
         })}
       </div>
 
-      <div style={{ width: "100%", textAlign: "center" }}>
-        <form onSubmit={submitHandler}>
-          <textarea
-            value={SendComment}
-            onChange={(e) => setSendComment(e.currentTarget.value)}
-            rows="3"
-          ></textarea>
-          <button type="submit">전송</button>
-        </form>
-      </div>
+      <ChatUpload ChatRoomId={ChatRoomId} user={user} />
+
+      <ImageUpload ChatRoomId={ChatRoomId}/>
     </>
   );
 }
