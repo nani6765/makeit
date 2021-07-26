29f2a907d9deacda80edd15ff6cd23c0f4df07af
nani6765 <nani6765@gmail.com>
2021-07-26 17:59:12 +0900

    hotfix

---
+++ config/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://MediaFriend:mfs0601!@cluster0.ssww1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  access_key: "CpjVKZ482dCyHh72SWEJ",
+  secret_key: "p7pB3ddHIsKDK5L2qfgufIqYWk8njr4JodBqNy4q",
+};
