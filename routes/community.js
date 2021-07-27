1760fbf0525c82a281477e63d5493545d3021cd8
nani6765 <nani6765@gmail.com>
2021-07-27 18:21:07 +0900

    test

--- routes/community.js
+++ routes/community.js
@@ -403,6 +403,117 @@ router.post("/rerepleGetAuther", (req, res) => {
     });
 });
 
+/*
+router.post("/rerepleSubmit", (req, res) => {
+  let temp = req.body;
+  let rereple = {};
+  rereple.postNum = temp.postNum;
+  User.findOne({ uid: temp.uid })
+    .exec()
+    .then((userInfo) => {
+      rereple.auther = userInfo._id;
+      rereple.content = temp.content;
+      rereple.realTime = moment().format("YY-MM-DD[ ]HH:mm");
+      const rerepleObj = new CommunityRereple(rereple);
+      rerepleObj.save((doc) => {
+        CommunityReple.findOneAndUpdate(
+          { _id: temp.repleInfo._id },
+          {
+            $inc: { rerepleNum: 1 },
+            $push: { rerepleArray: doc._id },
+          }
+        );
+      });
+    })
+    .exec()
+    .then(() => {
+      Community.findOneAndUpdate(
+        { postNum: temp.postNum },
+        { $inc: { repleNum: 1 } }
+      );
+    })
+    .exec()
+    .then((result) => {
+      let alarmtemp = {
+        uid: result.auther,
+        url: temp.postNum,
+        type: "rerepleToPost",
+      };
+      if (temp.uid != result.auther) {
+        const alarm = new Alarm(alarmtemp);
+        alarm.save();
+      }
+      if (temp.uid != temp.repleInfo._id) {
+        alarmtemp.uid = temp.repleInfo._id;
+        alarmtemp.type = "rerepleToreple";
+        const replealarm = new Alarm(alarmtemp);
+        replealarm.save(() => {
+          return res.status(200).send({ success: true });
+        });
+      }
+      return res.status(200).send({ success: true });
+    })
+    .catch((err) => {
+      console.log("err", err);
+      if (err) return res.status(400).json({ success: false, err });
+    });
+});
+*/
+
+router.post("/rerepleSubmit", (req, res) => {
+  let temp = req.body;
+  let rereple = {};
+  rereple.postNum = temp.postNum;
+  User.findOne({ uid: temp.uid })
+    .exec()
+    .then((userInfo) => {
+      rereple.auther = userInfo._id;
+      rereple.content = temp.content;
+      rereple.realTime = moment().format("YY-MM-DD[ ]HH:mm");
+      const rerepleObj = new CommunityRereple(rereple);
+      rerepleObj.save((doc) => {
+        CommunityReple.findOneAndUpdate(
+          { _id: temp.repleInfo._id },
+          {
+            $inc: { rerepleNum: 1 },
+            $push: { rerepleArray: doc._id },
+          }
+        ).exec();
+      });
+    })
+    .then(() => {
+      Community.findOneAndUpdate(
+        { postNum: req.body.postNum },
+        { $inc: { repleNum: 1 } }
+      )
+        .exec()
+        .then((result) => {
+          let alarmtemp = {
+            uid: result.auther,
+            url: temp.postNum,
+            type: "rerepleToPost",
+          };
+          if (req.body.uid != result.auther) {
+            const alarm = new Alarm(alarmtemp);
+            alarm.save();
+          }
+          if (req.body.uid != req.body.repleInfo._id) {
+            alarmtemp.uid = req.body.repleInfo._id;
+            alarmtemp.type = "rerepleToreple";
+            const replealarm = new Alarm(alarmtemp);
+            replealarm.save(() => {
+              return res.status(200).send({ success: true });
+            });
+          }
+          return res.status(200).send({ success: true });
+        });
+    })
+    .catch((err) => {
+      console.log("err", err);
+      if (err) return res.status(400).json({ success: false, err });
+    });
+});
+
 router.post("/rerepleSubmit", (req, res) => {
   let temp = req.body;
   let rereple = {};
