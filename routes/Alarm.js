f21671d4450223c62c97e389ee7952aa718e06cf
kimdoyoen <98ehdusdl@naver.com>
2021-07-27 19:49:56 +0900

    20210727

--- routes/Alarm.js
+++ routes/Alarm.js
@@ -2,7 +2,17 @@ var router = require("express").Router();
 const { Alarm } = require("../model/Alarm.js");
 
 router.post("/getAlarm", (req, res) => {
-
+    Alarm.find({uid: req.body.uid}).exec()
+    .then((response) => {
+        console.log(req.body.uid);
+      return res.status(200).send({
+        success: true,
+        alarms: response,
+      });
+    }).catch((err) => {
+      console.log(err);
+      return res.status(400).json({ success: false, err });
+    });
 });
 
 
