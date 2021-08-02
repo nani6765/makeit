a8f27ade04756a696071747d888239c16e50e531
nani6765 <nani6765@gmail.com>
2021-08-02 14:52:46 +0900

    Test

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use("/api/main", require("./routes/main.js"));
 app.use("/api/user", require("./routes/user.js"));
 app.use("/api/community", require("./routes/community.js"));
 app.use("/api/chat", require("./routes/chat.js"));
-app.use("/api/alarm", require("./routes/alarm.js"));
+app.use("/api/alarm", require("./routes/alarms.js"));
 
 const port = process.env.PORT || 5000;
 
