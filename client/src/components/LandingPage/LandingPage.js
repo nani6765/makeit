bb0fb776abfdb4c6b0526c36f12d7c54e89b7abe
nani6765 <nani6765@gmail.com>
2021-08-19 17:16:38 +0900

    20210819

--- client/src/components/LandingPage/LandingPage.js
+++ client/src/components/LandingPage/LandingPage.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Slider from "react-slick";
 
 import FirstLanding from "./views/FirstLanding.js";
@@ -7,16 +7,15 @@ import ThirdLanding from "./views/ThirdLanding.js";
 import FourthLanding from "./views/FourthLanding.js";
 import FifthLanding from "./views/FifthLanding.js";
 import SixthLanding from "./views/SixthLanding.js";
-import { last } from "lodash";
+
+import { PCOnly, PCExcept } from "./css/LandigPageElement.js";
 
 function LandingPage() {
   const sliderRef = useRef();
 
-  var NextKeys = {39: 1, 40: 1};
-  var PrevKeys = {37: 1, 38: 1};
+  var NextKeys = { 39: 1, 40: 1 };
+  var PrevKeys = { 37: 1, 38: 1 };
 
-  var lastY = 0;
- 
   var settings = {
     dots: false,
     infinite: false,
@@ -25,87 +24,80 @@ function LandingPage() {
     easing: "ease-in-out",
     vertical: true,
     slidesToShow: 1,
-    slidesToScroll: 1
+    slidesToScroll: 1,
   };
 
   const gotoNext = () => {
     sliderRef.current.slickNext();
-  }
+  };
 
   const gotoPrev = () => {
     sliderRef.current.slickPrev();
-  }
+  };
 
   function wheelEvent(e) {
     e.preventDefault();
-      if (e.deltaY < 0){
-         gotoPrev();
-      }
-      else if (e.deltaY > 0){
-          gotoNext();
-      }
+    if (e.deltaY < 0) {
+      gotoPrev();
+    } else if (e.deltaY > 0) {
+      gotoNext();
+    }
   }
 
   function keyEvent(e) {
     e.preventDefault();
     if (NextKeys[e.keyCode]) {
       gotoNext();
-    }
-    else if(PrevKeys[e.keyCode]){
+    } else if (PrevKeys[e.keyCode]) {
       gotoPrev();
     }
     return false;
   }
 
-  useEffect(() => {
-    console.log(lastY);
-  }, [lastY]);
-
- function touchEvent(e) {
-    let y = parseInt(e.touches[0].pageY);
-   console.log("y ", y, " lastY ", lastY);
-   
-    if (Math.abs(y - lastY) > 5) {
-        if (y > lastY) {
-          gotoNext(); 
-        } else {
-          gotoPrev();
-        }
-      return false;
-    }
-    lastY = y;
-     return false;
-  };
-  
   // call this to Disable
   function disableScroll() {
+    window.addEventListener(
+      "wheel",
+      (e) => {
+        wheelEvent(e);
+      },
+      { passive: false }
+    ); // modern desktop
 
-    window.addEventListener("wheel", (e) => {
-      wheelEvent(e);
-    },  { passive: false }); // modern desktop
-
-    window.addEventListener('keydown', (e) => {
-      keyEvent(e);
-    }, false);
-
-    window.addEventListener('touchmove', (e) => {
-      touchEvent(e);
-    },  { passive: false }); // mobile
+    window.addEventListener(
+      "keydown",
+      (e) => {
+        keyEvent(e);
+      },
+      false
+    );
   }
 
   useEffect(() => {
-    disableScroll()
-  }, [])
+    disableScroll();
+  }, []);
 
   return (
-    <Slider {...settings} ref={sliderRef}>
-      <FirstLanding/>
-      <SecondLanding/>
-      <ThirdLanding />
-      <FourthLanding />
-      <FifthLanding />
-      <SixthLanding />
-    </Slider>
+    <>
+      <PCOnly>
+        <Slider {...settings} ref={sliderRef}>
+          <FirstLanding />
+          <SecondLanding />
+          <ThirdLanding />
+          <FourthLanding />
+          <FifthLanding />
+          <SixthLanding />
+        </Slider>
+      </PCOnly>
+      <PCExcept>
+        <FirstLanding />
+        <SecondLanding />
+        <ThirdLanding />
+        <FourthLanding />
+        <FifthLanding />
+        <SixthLanding />
+      </PCExcept>
+    </>
   );
 }
 
--- client/src/components/LandingPage/css/LandigPageElement.js
+++ client/src/components/LandingPage/css/LandigPageElement.js
@@ -1,4 +1,4 @@
-  /** @jsxRuntime classic */
+/** @jsxRuntime classic */
 /** @jsx jsx */
 import { jsx, css } from "@emotion/react";
 import styled from "@emotion/styled";
@@ -6,44 +6,76 @@ import styled from "@emotion/styled";
 const breakpoints = [1200, 576];
 const mq = breakpoints.map((bp) => `@media (max-width: ${bp}px)`);
 
+const PCOnly = styled.div`
+  display: block;
+  ${mq[0]} {
+    display: none;
+  }
+`;
+
+const PCExcept = styled.div`
+  display: none;
+  ${mq[0]} {
+    display: block;
+  }
+`;
+
 const FirstDiv = styled.div`
   width: 100%;
-  height: 100vh;
-  background-image: url("/Img/landingPage/landing1.png");
+  height: calc(100vh - 80px);
+  background-image: url("https://kr.object.ncloudstorage.com/makeit/admin/LandingPage/Landing1.png");
   background-size: cover;
+  ${mq[1]} {
+    height: calc(100vh - 60px);
+  }
 `;
 const SecondDiv = styled.div`
   width: 100%;
-  height: 100vh;
-  background-image: url("/Img/landingPage/landing2.png");
+  height: calc(100vh - 80px);
+  background-image: url("https://kr.object.ncloudstorage.com/makeit/admin/LandingPage/Landing2.png");
   background-size: cover;
+  ${mq[1]} {
+    height: calc(100vh - 60px);
+  }
 `;
 
 const ThirdDiv = styled.div`
   width: 100%;
-  height: 100vh;
-  background-image: url("/Img/landingPage/landing3.png");
+  height: calc(100vh - 80px);
+  background-image: url("https://kr.object.ncloudstorage.com/makeit/admin/LandingPage/Landing3.png");
   background-size: cover;
+  ${mq[1]} {
+    height: calc(100vh - 60px);
+  }
 `;
 const FourthDiv = styled.div`
   width: 100%;
-  height: 100vh;
-  background-image: url("/Img/landingPage/landing4.png");
+  height: calc(100vh - 80px);
+  background-image: url("https://kr.object.ncloudstorage.com/makeit/admin/LandingPage/Landing4.png");
   background-size: cover;
+  ${mq[1]} {
+    height: calc(100vh - 60px);
+  }
 `;
 
 const FifthDiv = styled.div`
   width: 100%;
-  height: 100vh;
-  background-image: url("/Img/landingPage/landing5.png");
+  height: calc(100vh - 80px);
+  background-image: url("https://kr.object.ncloudstorage.com/makeit/admin/LandingPage/Landing5.png");
   background-size: cover;
+  ${mq[1]} {
+    height: calc(100vh - 60px);
+  }
 `;
 
 const SixthDiv = styled.div`
   width: 100%;
-  height: 100vh;
-  background-image: url("/Img/landingPage/landing6.png");
+  height: calc(100vh - 80px);
+  background-image: url("https://kr.object.ncloudstorage.com/makeit/admin/LandingPage/Landing6.png");
   background-size: cover;
+  ${mq[1]} {
+    height: calc(100vh - 60px);
+  }
 `;
 
 export { FirstDiv, SecondDiv, ThirdDiv, FourthDiv, FifthDiv, SixthDiv };
