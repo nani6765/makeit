8d2afdba794c0dfafc68e05afd2b358b9f6abfc7
kimdoyoen <98ehdusdl@naver.com>
2021-08-20 15:29:46 +0900

    20210820

--- client/src/components/LandingPage/css/LandigPageElement.js
+++ client/src/components/LandingPage/css/LandigPageElement.js
@@ -1,4 +1,4 @@
-  /** @jsxRuntime classic */
+/** @jsxRuntime classic */
 /** @jsx jsx */
 import { jsx, css } from "@emotion/react";
 import styled from "@emotion/styled";
@@ -7,58 +7,84 @@ const breakpoints = [1200, 576];
 const mq = breakpoints.map((bp) => `@media (max-width: ${bp}px)`);
 
 const PCOnly = styled.div`
-  display : block;
-  ${mq[0]}{
+  display: block;
+  ${mq[0]} {
     display: none;
   }
 `;
 
 const PCExcept = styled.div`
-  display : none;
-  ${mq[0]}{
+  display: none;
+  ${mq[0]} {
     display: block;
   }
 `;
 
-
 const FirstDiv = styled.div`
   width: 100%;
-  height: 100vh;
-  background-image: url("/Img/landingPage/landing1.png");
+  height: calc(100vh - 80px);
+  background-image: url("https://kr.object.ncloudstorage.com/makeit/admin/LandingPage/Landing1.png");
   background-size: cover;
+  ${mq[1]} {
+    height: calc(100vh - 60px);
+  }
 `;
 const SecondDiv = styled.div`
   width: 100%;
-  height: 100vh;
-  background-image: url("/Img/landingPage/landing2.png");
+  height: calc(100vh - 80px);
+  background-image: url("https://kr.object.ncloudstorage.com/makeit/admin/LandingPage/Landing2.png");
   background-size: cover;
+  ${mq[1]} {
+    height: calc(100vh - 60px);
+  }
 `;
 
 const ThirdDiv = styled.div`
   width: 100%;
-  height: 100vh;
-  background-image: url("/Img/landingPage/landing3.png");
+  height: calc(100vh - 80px);
+  background-image: url("https://kr.object.ncloudstorage.com/makeit/admin/LandingPage/Landing3.png");
   background-size: cover;
+  ${mq[1]} {
+    height: calc(100vh - 60px);
+  }
 `;
 const FourthDiv = styled.div`
   width: 100%;
-  height: 100vh;
-  background-image: url("/Img/landingPage/landing4.png");
+  height: calc(100vh - 80px);
+  background-image: url("https://kr.object.ncloudstorage.com/makeit/admin/LandingPage/Landing4.png");
   background-size: cover;
+  ${mq[1]} {
+    height: calc(100vh - 60px);
+  }
 `;
 
 const FifthDiv = styled.div`
   width: 100%;
-  height: 100vh;
-  background-image: url("/Img/landingPage/landing5.png");
+  height: calc(100vh - 80px);
+  background-image: url("https://kr.object.ncloudstorage.com/makeit/admin/LandingPage/Landing5.png");
   background-size: cover;
+  ${mq[1]} {
+    height: calc(100vh - 60px);
+  }
 `;
 
 const SixthDiv = styled.div`
   width: 100%;
-  height: 100vh;
-  background-image: url("/Img/landingPage/landing6.png");
+  height: calc(100vh - 80px);
+  background-image: url("https://kr.object.ncloudstorage.com/makeit/admin/LandingPage/Landing6.png");
   background-size: cover;
+  ${mq[1]} {
+    height: calc(100vh - 60px);
+  }
 `;
 
-export { PCOnly, PCExcept, FirstDiv, SecondDiv, ThirdDiv, FourthDiv, FifthDiv, SixthDiv };
+export {
+  PCOnly,
+  PCExcept,
+  FirstDiv,
+  SecondDiv,
+  ThirdDiv,
+  FourthDiv,
+  FifthDiv,
+  SixthDiv,
+};
