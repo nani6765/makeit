157855902aaf3158df1445d61e751383e993827e
kimdoyoen <98ehdusdl@naver.com>
2021-08-23 18:44:57 +0900

    20210823

--- routes/community.js
+++ routes/community.js
@@ -94,4 +94,142 @@ router.post("/image", setUpload("makeit/community"), (req, res, next) => {
   });
 });
 
+router.post("/like", (req, res) => {
+  let key = req.body.likeFlag;
+
+  if(key) { // 좋아요를 이미 누른 상태
+    Community.findOneAndUpdate(
+      { postNum : req.body.postNum },
+      { $inc: {likeNum: -1}, $pull: {likeArray: req.body.userId} },
+    )
+    .exec()
+    .then((response) => {
+      return res.status(200).send({ success: true });
+    })
+    .catch((err) => {
+      console.log(err);
+      return res.status(400).json({ success: false, err });
+    })
+  }
+  else {
+    Community.findOneAndUpdate(
+      { postNum : req.body.postNum },
+      { $inc: {likeNum: 1}, $push: {likeArray: req.body.userId} },
+    )
+    .exec()
+    .then((response) => {
+      if(response.uid != req.body.userId) {
+        let alarmTemp = {
+          uid: response.uid,
+          url: req.body.postNum,
+          type: "likeToPost",
+          category: "community/post",
+        };
+        const alarm = new Alarm(alarmTemp);
+        alarm.save(() => {
+          return res.status(200).send({ success: true });
+        })
+      }
+      else {
+        return res.status(200).send({ success: true });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      return res.status(400).json({ success: false, err });
+    })
+  }
+})
+
+
+//reple
+
+router.post("/repleSubmit", (req, res) => {
+  let reple = {
+    uid: req.body.uid,
+    postNum: req.body.postNum,
+    content: req.body.content,
+  }
+
+  User.findOne({uid: req.body.uid}).exec()
+  .then((userInfo) => {
+    reple.auther = userInfo._id;
+    reple.realTime = moment().format("YY-MM-DD[ ]HH:mm");
+    const communityReple = new CommunityReple(reple);
+    communityReple.save(() => {
+      Community.findOneAndUpdate({postNum: req.body.postNum}, {$inc: {repleNum : 1}})
+      .exec()
+      .then((response) => {
+        if(response.uid != reple.uid) {
+          let alarmTemp = {
+            uid: response.uid,
+            url: reple.postNum,
+            type: "repleToPost",
+            category: "community/post",
+          }
+
+          const alarm = new Alarm(alarmTemp);
+          alarm.save(() => {
+            return res.status(200).send({ success: true });
+          });
+        }
+        else {
+          return res.status(200).send({ success: true });
+        }
+      })
+    })
+  })
+  .catch((err) => {
+    console.log(err);
+    return res.status(400).json({ success: false, err });
+  })
+});
+
+router.post("/repleLike", (req, res) => {
+  let key = req.body.likeFlag;
+
+  if(key) { // 좋아요를 이미 누른 상태
+    CommunityReple.findOneAndUpdate(
+      {_id: req.body.repleId},
+      { $inc: {likeNum: -1}, $pull: {likeArray: req.body.userId} },
+    )
+    .exec()
+    .then((response) => {
+      return res.status(200).send({ success: true });
+    })
+    .catch((err) => {
+      console.log(err);
+      return res.status(400).json({ success: false, err });
+    })
+  }
+  else {
+    CommunityReple.findOneAndUpdate(
+      {_id: req.body.repleId},
+      { $inc: {likeNum: 1}, $push: {likeArray: req.body.userId} },
+    )
+    .exec()
+    .then((response) => {
+      if(response.uid != req.body.userId) {
+        let alarmTemp = {
+          uid: response.uid,
+          url: response.postNum,
+          type: "likeToReple",
+          category: "community/post",
+        };
+        const alarm = new Alarm(alarmTemp);
+        alarm.save(() => {
+          return res.status(200).send({ success: true });
+        })
+      }
+      else {
+        return res.status(200).send({ success: true });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      return res.status(400).json({ success: false, err });
+    })
+  }
+})
+
 module.exports = router;
