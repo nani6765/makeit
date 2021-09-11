9dadd420794736739dcb0d5598fe6b40243cea99
nani6765 <nani6765@gmail.com>
2021-09-11 10:21:01 +0900

    20210911

--- client/src/components/MakingMedia/view/FindingProducer/ProducerList.js
+++ client/src/components/MakingMedia/view/FindingProducer/ProducerList.js
@@ -1,64 +1,276 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios';
+import React from "react";
 
 import { ProducerListContainer } from "../../css/FindingProducerCSS.js";
 
-function ProducerList(props) {
-    const [PostList, setPostList] = useState([]);
-
-    const loadPostList = async () => {
-        try {
-            let body = {
-                category: props.SubCategory,
-                sort: props.Sort,
-                skip: props.Skip,
-            }
-
-            axios.post("/api/making/producer", body).then((response) => {
-                if(response.data.success) {
-                    let temp = [...response.data.posts]
-                    setPostList(temp);
-                }
-            })
-            
-        } catch (error) {
-            console.log("loadPostList error", error)
-        }
-    }
-
-    useEffect(() => {
-        loadPostList();
-    }, [props.SubCategory, props.Sort, props.Skip]);
-
-    return (
-        <ProducerListContainer>
-            {
-                PostList.map((post, idx) => {
-                    return (
-                        <div className="producercard" key={idx}>
-                            <img src={post.thumbnailArr[0].path} className="thumbnail" />
-                            <div className="info">
-                                <span className="producerName">{post.auther.displayName}</span>
-                                {
-                                    post.likeArray.indexOf(props.user.userData.uid) === -1
-                                    ? <i className="bi bi-heart"></i>
-                                    : <i className="bi bi-heart-fill"></i>
-                                }
-                            </div>
-                            <p className="title">{post.oneLineIntroduce}</p>
-                            <div className="priceAndGrade">
-                                <p className="price">{post.priceInfo}</p>
-                                <p className="grade">
-                                    <i className="bi bi-star"></i>
-                                    {post.grade} | {post.gradeArray.length}개의 평가
-                                </p>
-                            </div>
-                        </div>
-                    )
-                })
-            }
-        </ProducerListContainer>
-    )
+function ProducerList() {
+  return (
+    <ProducerListContainer>
+      <div className="producercard">
+        <img src="/Img/MainBanner.jpg" className="thumbnail" />
+        <div className="info">
+          <span className="producerName">프로제작러</span>
+          <i className="bi bi-heart"></i>
+        </div>
+        <p className="title">
+          vlog, 일상, 여행 영상 최고의 가성비로 해드립니다. vlog, 일상, 여행
+          영상 최고의 가성비로 해드립니다. vlog, 일상, 여행 영상 최고의 가성비로
+          해드립니다. vlog, 일상, 여행 영상 최고의 가성비로 해드립니다. vlog,
+          일상, 여행 영상 최고의 가성비로 해드립니다. vlog, 일상, 여행 영상
+          최고의 가성비로 해드립니다. vlog, 일상, 여행 영상 최고의 가성비로
+          해드립니다.
+        </p>
+        <div className="priceAndGrade">
+          <p className="price">300,000원</p>
+          <p className="grade">
+            <i className="bi bi-star"></i>
+            3.0 | 3개의 평가
+          </p>
+        </div>
+      </div>
+      <div className="producercard">
+        <img src="/Img/MainBanner.jpg" className="thumbnail" />
+        <div className="info">
+          <span className="producerName">프로제작러</span>
+          <i className="bi bi-heart"></i>
+        </div>
+        <p className="title">
+          vlog, 일상, 여행 영상 최고의 가성비로 해드립니다. vlog, 일상, 여행
+          영상 최고의 가성비로 해드립니다. vlog, 일상, 여행 영상 최고의 가성비로
+          해드립니다. vlog, 일상, 여행 영상 최고의 가성비로 해드립니다. vlog,
+          일상, 여행 영상 최고의 가성비로 해드립니다. vlog, 일상, 여행 영상
+          최고의 가성비로 해드립니다. vlog, 일상, 여행 영상 최고의 가성비로
+          해드립니다.
+        </p>
+        <div className="priceAndGrade">
+          <p className="price">300,000원</p>
+          <p className="grade">
+            <i className="bi bi-star"></i>
+            3.0 | 3개의 평가
+          </p>
+        </div>
+      </div>
+      <div className="producercard">
+        <img src="/Img/MainBanner.jpg" className="thumbnail" />
+        <div className="info">
+          <span className="producerName">프로제작러</span>
+          <i className="bi bi-heart"></i>
+        </div>
+        <p className="title">
+          vlog, 일상, 여행 영상 최고의 가성비로 해드립니다. vlog, 일상, 여행
+          영상 최고의 가성비로 해드립니다. vlog, 일상, 여행 영상 최고의 가성비로
+          해드립니다. vlog, 일상, 여행 영상 최고의 가성비로 해드립니다. vlog,
+          일상, 여행 영상 최고의 가성비로 해드립니다. vlog, 일상, 여행 영상
+          최고의 가성비로 해드립니다. vlog, 일상, 여행 영상 최고의 가성비로
+          해드립니다.
+        </p>
+        <div className="priceAndGrade">
+          <p className="price">300,000원</p>
+          <p className="grade">
+            <i className="bi bi-star"></i>
+            3.0 | 3개의 평가
+          </p>
+        </div>
+      </div>
+      <div className="producercard">
+        <img src="/Img/MainBanner.jpg" className="thumbnail" />
+        <div className="info">
+          <span className="producerName">프로제작러</span>
+          <i className="bi bi-heart"></i>
+        </div>
+        <p className="title">
+          vlog, 일상, 여행 영상 최고의 가성비로 해드립니다. vlog, 일상, 여행
+          영상 최고의 가성비로 해드립니다. vlog, 일상, 여행 영상 최고의 가성비로
+          해드립니다. vlog, 일상, 여행 영상 최고의 가성비로 해드립니다. vlog,
+          일상, 여행 영상 최고의 가성비로 해드립니다. vlog, 일상, 여행 영상
+          최고의 가성비로 해드립니다. vlog, 일상, 여행 영상 최고의 가성비로
+          해드립니다.
+        </p>
+        <div className="priceAndGrade">
+          <p className="price">300,000원</p>
+          <p className="grade">
+            <i className="bi bi-star"></i>
+            3.0 | 3개의 평가
+          </p>
+        </div>
+      </div>
+      <div className="producercard">
+        <img src="/Img/MainBanner.jpg" className="thumbnail" />
+        <div className="info">
+          <span className="producerName">프로제작러</span>
+          <i className="bi bi-heart"></i>
+        </div>
+        <p className="title">
+          vlog, 일상, 여행 영상 최고의 가성비로 해드립니다. vlog, 일상, 여행
+          영상 최고의 가성비로 해드립니다. vlog, 일상, 여행 영상 최고의 가성비로
+          해드립니다. vlog, 일상, 여행 영상 최고의 가성비로 해드립니다. vlog,
+          일상, 여행 영상 최고의 가성비로 해드립니다. vlog, 일상, 여행 영상
+          최고의 가성비로 해드립니다. vlog, 일상, 여행 영상 최고의 가성비로
+          해드립니다.
+        </p>
+        <div className="priceAndGrade">
+          <p className="price">300,000원</p>
+          <p className="grade">
+            <i className="bi bi-star"></i>
+            3.0 | 3개의 평가
+          </p>
+        </div>
+      </div>
+      <div className="producercard">
+        <img src="/Img/MainBanner.jpg" className="thumbnail" />
+        <div className="info">
+          <span className="producerName">프로제작러</span>
+          <i className="bi bi-heart"></i>
+        </div>
+        <p className="title">
+          vlog, 일상, 여행 영상 최고의 가성비로 해드립니다. vlog, 일상, 여행
+          영상 최고의 가성비로 해드립니다. vlog, 일상, 여행 영상 최고의 가성비로
+          해드립니다. vlog, 일상, 여행 영상 최고의 가성비로 해드립니다. vlog,
+          일상, 여행 영상 최고의 가성비로 해드립니다. vlog, 일상, 여행 영상
+          최고의 가성비로 해드립니다. vlog, 일상, 여행 영상 최고의 가성비로
+          해드립니다.
+        </p>
+        <div className="priceAndGrade">
+          <p className="price">300,000원</p>
+          <p className="grade">
+            <i className="bi bi-star"></i>
+            3.0 | 3개의 평가
+          </p>
+        </div>
+      </div>
+      <div className="producercard">
+        <img src="/Img/MainBanner.jpg" className="thumbnail" />
+        <div className="info">
+          <span className="producerName">프로제작러</span>
+          <i className="bi bi-heart"></i>
+        </div>
+        <p className="title">
+          vlog, 일상, 여행 영상 최고의 가성비로 해드립니다. vlog, 일상, 여행
+          영상 최고의 가성비로 해드립니다. vlog, 일상, 여행 영상 최고의 가성비로
+          해드립니다. vlog, 일상, 여행 영상 최고의 가성비로 해드립니다. vlog,
+          일상, 여행 영상 최고의 가성비로 해드립니다. vlog, 일상, 여행 영상
+          최고의 가성비로 해드립니다. vlog, 일상, 여행 영상 최고의 가성비로
+          해드립니다.
+        </p>
+        <div className="priceAndGrade">
+          <p className="price">300,000원</p>
+          <p className="grade">
+            <i className="bi bi-star"></i>
+            3.0 | 3개의 평가
+          </p>
+        </div>
+      </div>
+      <div className="producercard">
+        <img src="/Img/MainBanner.jpg" className="thumbnail" />
+        <div className="info">
+          <span className="producerName">프로제작러</span>
+          <i className="bi bi-heart"></i>
+        </div>
+        <p className="title">
+          vlog, 일상, 여행 영상 최고의 가성비로 해드립니다. vlog, 일상, 여행
+          영상 최고의 가성비로 해드립니다. vlog, 일상, 여행 영상 최고의 가성비로
+          해드립니다. vlog, 일상, 여행 영상 최고의 가성비로 해드립니다. vlog,
+          일상, 여행 영상 최고의 가성비로 해드립니다. vlog, 일상, 여행 영상
+          최고의 가성비로 해드립니다. vlog, 일상, 여행 영상 최고의 가성비로
+          해드립니다.
+        </p>
+        <div className="priceAndGrade">
+          <p className="price">300,000원</p>
+          <p className="grade">
+            <i className="bi bi-star"></i>
+            3.0 | 3개의 평가
+          </p>
+        </div>
+      </div>
+      <div className="producercard">
+        <img src="/Img/MainBanner.jpg" className="thumbnail" />
+        <div className="info">
+          <span className="producerName">프로제작러</span>
+          <i className="bi bi-heart"></i>
+        </div>
+        <p className="title">
+          vlog, 일상, 여행 영상 최고의 가성비로 해드립니다. vlog, 일상, 여행
+          영상 최고의 가성비로 해드립니다. vlog, 일상, 여행 영상 최고의 가성비로
+          해드립니다. vlog, 일상, 여행 영상 최고의 가성비로 해드립니다. vlog,
+          일상, 여행 영상 최고의 가성비로 해드립니다. vlog, 일상, 여행 영상
+          최고의 가성비로 해드립니다. vlog, 일상, 여행 영상 최고의 가성비로
+          해드립니다.
+        </p>
+        <div className="priceAndGrade">
+          <p className="price">300,000원</p>
+          <p className="grade">
+            <i className="bi bi-star"></i>
+            3.0 | 3개의 평가
+          </p>
+        </div>
+      </div>
+      <div className="producercard">
+        <img src="/Img/MainBanner.jpg" className="thumbnail" />
+        <div className="info">
+          <span className="producerName">프로제작러</span>
+          <i className="bi bi-heart"></i>
+        </div>
+        <p className="title">
+          vlog, 일상, 여행 영상 최고의 가성비로 해드립니다. vlog, 일상, 여행
+          영상 최고의 가성비로 해드립니다. vlog, 일상, 여행 영상 최고의 가성비로
+          해드립니다. vlog, 일상, 여행 영상 최고의 가성비로 해드립니다. vlog,
+          일상, 여행 영상 최고의 가성비로 해드립니다. vlog, 일상, 여행 영상
+          최고의 가성비로 해드립니다. vlog, 일상, 여행 영상 최고의 가성비로
+          해드립니다.
+        </p>
+        <div className="priceAndGrade">
+          <p className="price">300,000원</p>
+          <p className="grade">
+            <i className="bi bi-star"></i>
+            3.0 | 3개의 평가
+          </p>
+        </div>
+      </div>
+      <div className="producercard">
+        <img src="/Img/MainBanner.jpg" className="thumbnail" />
+        <div className="info">
+          <span className="producerName">프로제작러</span>
+          <i className="bi bi-heart"></i>
+        </div>
+        <p className="title">
+          vlog, 일상, 여행 영상 최고의 가성비로 해드립니다. vlog, 일상, 여행
+          영상 최고의 가성비로 해드립니다. vlog, 일상, 여행 영상 최고의 가성비로
+          해드립니다. vlog, 일상, 여행 영상 최고의 가성비로 해드립니다. vlog,
+          일상, 여행 영상 최고의 가성비로 해드립니다. vlog, 일상, 여행 영상
+          최고의 가성비로 해드립니다. vlog, 일상, 여행 영상 최고의 가성비로
+          해드립니다.
+        </p>
+        <div className="priceAndGrade">
+          <p className="price">300,000원</p>
+          <p className="grade">
+            <i className="bi bi-star"></i>
+            3.0 | 3개의 평가
+          </p>
+        </div>
+      </div>
+      <div className="producercard">
+        <img src="/Img/MainBanner.jpg" className="thumbnail" />
+        <div className="info">
+          <span className="producerName">프로제작러</span>
+          <i className="bi bi-heart"></i>
+        </div>
+        <p className="title">
+          vlog, 일상, 여행 영상 최고의 가성비로 해드립니다. vlog, 일상, 여행
+          영상 최고의 가성비로 해드립니다. vlog, 일상, 여행 영상 최고의 가성비로
+          해드립니다. vlog, 일상, 여행 영상 최고의 가성비로 해드립니다. vlog,
+          일상, 여행 영상 최고의 가성비로 해드립니다. vlog, 일상, 여행 영상
+          최고의 가성비로 해드립니다. vlog, 일상, 여행 영상 최고의 가성비로
+          해드립니다.
+        </p>
+        <div className="priceAndGrade">
+          <p className="price">300,000원</p>
+          <p className="grade">
+            <i className="bi bi-star"></i>
+            3.0 | 3개의 평가
+          </p>
+        </div>
+      </div>
+    </ProducerListContainer>
+  );
 }
 
-export default ProducerList
+export default ProducerList;
--- client/src/components/MakingMedia/view/FindingProducer/upload/FindingProducerUpload.js
+++ client/src/components/MakingMedia/view/FindingProducer/upload/FindingProducerUpload.js
@@ -26,6 +26,7 @@ function FindingProducerUpload(props) {
     { idx: 3, value: "수정/환불안내" },
   ];
   const [CurrentProcess, setCurrentProcess] = useState("상세설명");
+  const [CureentIdx, setCureentIdx] = useState(0);
 
   //상세설명
   const [OneLineIntroduce, setOneLineIntroduce] = useState("");
@@ -46,6 +47,110 @@ function FindingProducerUpload(props) {
   //수정환불안내
   const [EditandReprogress, setEditandReprogress] = useState("");
   const [FAQList, setFAQList] = useState([{ q: "", a: "" }]);
+
+  useEffect(() => {
+    console.log(FAQList);
+  }, [FAQList]);
+
+  const LoadTempPost = async () => {
+    try {
+      await axios
+        .post("/api/making/producer/loadTempPost", { uid: user.userData.uid })
+        .then((response) => {
+          console.log(response.data);
+          if (response.data.success && response.data.tempPost) {
+            console.log(response.data);
+            //datail
+            setOneLineIntroduce(response.data.tempPost.oneLineIntroduce);
+            setCategory(response.data.tempPost.category);
+            setDescription(response.data.tempPost.description);
+            setWorkTypeArr(response.data.tempPost.workTypeArr);
+            setVideoPurposeArr(response.data.tempPost.videoPurposeArr);
+
+            //Portfolio
+            setThumbnailArr(response.data.tempPost.thumbnailArr);
+            setDetailImgArr(response.data.tempPost.detailImgArr);
+            setVideoArr(response.data.tempPost.videoArr);
+
+            //price
+            setPriceInfo(response.data.tempPost.priceInfo);
+            setPriceDirectInput(response.data.tempPost.priceDirectInput);
+
+            //confirm
+            setEditandReprogress(response.data.tempPost.editandReprogress);
+            setFAQList(response.data.tempPost.FAQList);
+          }
+        });
+    } catch (error) {
+      alert("임시저장 불러오기 실패");
+      console.log("임시저장 error", error);
+    }
+  };
+
+  useEffect(() => {
+    LoadTempPost();
+  }, []);
+
+  const TempSaveHandler = () => {
+    let body = {
+      uid: user.userData.uid,
+      email: user.userData.email,
+      oneLineIntroduce: OneLineIntroduce,
+      category: Category,
+      description: Description,
+      workTypeArr: WorkTypeArr,
+      videoPurposeArr: VideoPurposeArr,
+      thumbnailArr: ThumbnailArr,
+      detailImgArr: DetailImgArr,
+      videoArr: VideoArr,
+      priceInfo: PriceInfo,
+      priceDirectInput: PriceDirectInput,
+      editandReprogress: EditandReprogress,
+      FAQList: FAQList,
+    };
+    axios
+      .post("/api/making/producer/tempSaving", body)
+      .then((response, err) => {
+        if (response.data.success) {
+          alert("임시 저장이 완료되었습니다.");
+        } else {
+          alert("임시 저장이 실패했습니다.");
+          console.log("임시저장 error", err);
+        }
+      });
+  };
+
+  const SubmitHandler = () => {
+    let body = {
+      uid: user.userData.uid,
+      email: user.userData.email,
+      oneLineIntroduce: OneLineIntroduce,
+      category: Category,
+      description: Description,
+      workTypeArr: WorkTypeArr,
+      videoPurposeArr: VideoPurposeArr,
+      thumbnailArr: ThumbnailArr,
+      detailImgArr: DetailImgArr,
+      videoArr: VideoArr,
+      priceInfo: PriceInfo,
+      priceDirectInput: PriceDirectInput,
+      editandReprogress: EditandReprogress,
+      FAQList: FAQList,
+    };
+    axios
+      .post("/api/making/producer/proPostSubmit", body)
+      .then((response, err) => {
+        if (response.data.success) {
+          alert("제작자 게시가 완료되었습니다.");
+          props.history.push({
+            pathname: "/making",
+          });
+        } else {
+          alert("제작자 게시에 실패했습니다.");
+          console.log("prosubmit error", err);
+        }
+      });
+  };
 
   const setRightContent = () => {
     switch (CurrentProcess) {
