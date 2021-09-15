4f682e0c6441feb947b874989645165704882b91
nani6765 <nani6765@gmail.com>
2021-09-15 13:55:04 +0900

    20210915

--- client/src/components/MakingMedia/MakingMedia.js
+++ client/src/components/MakingMedia/MakingMedia.js
@@ -1,18 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import HeaderGNB from "./view/common/HeaderGNB";
 import FindingProducer from "./view/FindingProducer/FindingProducer.js";
 import { MakingDiv, DescriptionDiv } from "./css/CommonCSS.js";
 
 function MakingMedia() {
-  const user = useSelector((state) => state.user);
+  const user = useSelector((state) => state.user.userData);
+  useEffect(() => {
+    console.log("user : ", user);
+  }, []);
 
   const [Menu, setMenu] = useState("영상 제작자 탐색");
 
   const setContent = () => {
     switch (Menu) {
       case "영상 제작자 탐색":
-        return <FindingProducer Menu={Menu} user={user}/>;
+        return <FindingProducer Menu={Menu} user={user} />;
 
       default:
         break;
@@ -21,14 +24,15 @@ function MakingMedia() {
 
   return (
     <>
-    <DescriptionDiv>
-      영상 제작에 적합한 업체 혹은 프로를 찾아보거나,<br />
-      영상을 의뢰해서 업체들과 프로에게 견적을 받아보세요!
-    </DescriptionDiv>
-    <MakingDiv>
-      <HeaderGNB Menu={Menu} setMenu={setMenu} />
-      {setContent()}
-    </MakingDiv>
+      <DescriptionDiv>
+        영상 제작에 적합한 업체 혹은 프로를 찾아보거나,
+        <br />
+        영상을 의뢰해서 업체들과 프로에게 견적을 받아보세요!
+      </DescriptionDiv>
+      <MakingDiv>
+        <HeaderGNB Menu={Menu} setMenu={setMenu} />
+        {setContent()}
+      </MakingDiv>
     </>
   );
 }
