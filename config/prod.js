8713f436635b619dcf0682995bfcb845c81df3f0
nani6765 <nani6765@gmail.com>
2021-10-25 14:02:00 +0900

    20211025

--- config/prod.js
+++ config/prod.js
@@ -2,4 +2,8 @@ module.exports = {
   mongoURI: process.env.MONGO_URI,
   access_key: process.env.S3_KEY,
   secret_key: process.env.S3_SECRET,
+  smtpServerURL: process.env.MAIL_SMTP_URL,
+  authUser: process.env.MAIL_AUTHUSER,
+  authPass: process.env.MAIL_AUTHPASS,
+  port: process.env.MAIL_PORT,
 };
